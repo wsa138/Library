26578742b957f5c124c0fb276ddc6fe544083da2
William Addesso <wsaddesso@gmail.com>
2020-12-07 01:28:55 -0500

    Added constructor function for Book object. Added myLibrary array. Added info function to Book object prototype. Add Book button now either hides or displays the add book form.

---
+++ script.js
@@ -0,0 +1,41 @@
+let myLibrary = [];
+
+//Constructor function for Book
+function Book(title, author, pages, status) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.status = status;
+}
+
+// Add info function to Book object
+Book.prototype.info = function() {
+    console.log(`${this.title}, ${this.author}, ${this.pages}, ${this.status}`);
+    console.log("running info().")
+}
+
+function addBookToLibrary(book) {
+    myLibrary.push(book);
+    console.log("Add book to array.")
+}
+
+let addBookForm = document.getElementById("addBookForm");
+
+// Function that opens the form to add a book
+function openForm() {
+    if (addBookForm.style.display === "none") {
+        addBookForm.style.display = "block";
+    } else {
+        addBookForm.style.display = "none"
+    }
+}
+
+
+
+// Sample books
+let book1 = new Book("LOTR", "JRRT", "500", "Completed");
+let book2 = new Book("LHarry Potter", "JK R", "200", "Unread");
+
+//Add sample books to array
+addBookToLibrary(book1);
+addBookToLibrary(book2);
