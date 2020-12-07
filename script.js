ab9bca73c47bfc2b868c0fe0f57c7e49b8266cf0
William Addesso <wsaddesso@gmail.com>
2020-12-07 01:39:05 -0500

    Added function to close form when submit is clicked.

--- script.js
+++ script.js
@@ -23,15 +23,24 @@ let addBookForm = document.getElementById("addBookForm");
 
 // Function that opens the form to add a book
 function openForm() {
-    if (addBookForm.style.display === "none") {
-        addBookForm.style.display = "block";
-    } else {
-        addBookForm.style.display = "none"
-    }
+    addBookForm.style.display = "block";
+}
+
+
+// Creates a new book object with info from form
+function createNewBook() {
+    closeForm();
+}
+
+// Closes the add book form when submit button is clicked
+function closeForm() {
+    addBookForm.style.display = "none";
 }
 
 
 
+
+/*
 // Sample books
 let book1 = new Book("LOTR", "JRRT", "500", "Completed");
 let book2 = new Book("LHarry Potter", "JK R", "200", "Unread");
@@ -39,3 +48,4 @@ let book2 = new Book("LHarry Potter", "JK R", "200", "Unread");
 //Add sample books to array
 addBookToLibrary(book1);
 addBookToLibrary(book2);
+*/
